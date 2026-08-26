31c48117919c261c0686e42142465f174fcaea7c
Kaushal <jhakaushal.1809@gmail.com>
2026-08-26 10:28:36 +0530

    Separate task list and create views

--- tasks/views.py
+++ tasks/views.py
@@ -2,21 +2,7 @@ from django.shortcuts import redirect, render
 from .models import Task
 
 
-def home(request):
-
-    if request.method == "POST":
-        title = request.POST.get("title")
-        description = request.POST.get("description")
-        priority = request.POST.get("priority")
-
-        Task.objects.create(
-            title=title,
-            description=description,
-            priority = priority
-        )
-
-        return redirect("home")
-
+def task_list(request):
     tasks = Task.objects.all()
 
     context = {
@@ -25,3 +11,20 @@ def home(request):
     }
 
     return render(request, "tasks/home.html", context)
+
+def task_create(request):
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        priority = request.POST.get("priority")
+
+        Task.objects.create(
+            title = title,
+            description = description,
+            priority = priority
+        )
+
+        return redirect("task_list")
+
+    return render(request, "tasks/task_form.html")
