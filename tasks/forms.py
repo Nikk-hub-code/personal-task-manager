e6acd82ff375cdaf7b1010ed250f695ddaff666f
Kaushal <jhakaushal.1809@gmail.com>
2026-08-27 09:57:13 +0530

    Add task form validation

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
+
     class Meta:
         model = Task
         fields = [
@@ -10,3 +13,27 @@ class TaskForm(forms.ModelForm):
             "priority",
             "completed",
         ]
+
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+
+        title = title.strip()
+
+        if len(title) < 3:
+            raise forms.ValidationError(
+                "Task title must contain at least 3 characters."
+            )
+
+        return title
+
+    def clean_description(self):
+        description = self.cleaned_data.get("description", "")
+
+        description = description.strip()
+
+        if description and len(description) < 5:
+            raise forms.ValidationError(
+                "Description must contain at least 5 characters."
+            )
+
+        return description
